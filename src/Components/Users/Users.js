c67f00ac6c005371d8d164b9dd54b181ab1fa3c7
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-04-09 14:56:39 +0600

    leading indicator added

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -4,29 +4,42 @@ import "./Users.css";
 const Users = () => {
   const [pages, setPages] = useState([]);
   const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   console.log(user);
 
   useEffect(() => {
     fetch("https://reqres.in/api/users?page=2")
       .then((res) => res.json())
-      .then((data) => setPages(data.data));
+      .then((data) => {
+        setPages(data.data);
+      });
   }, []);
 
   const handleCLick = (id) => {
+    setLoading(true);
     fetch(`https://reqres.in/api/users/${id}`)
       .then((res) => res.json())
-      .then((data) => setUser(data.data));
+      .then((data) => {
+        setUser(data.data);
+        setLoading(false);
+      });
   };
 
   return (
     <div>
       <div className="user-card">
-        <img src={user.avatar} alt="" />
-        <h2>
-          Name: {user.first_name} {user.last_name}
-        </h2>
-        <p>Email: {user.email}</p>
+        {loading ? (
+          "loading . . ."
+        ) : (
+          <>
+            <img src={user.avatar} alt="" />
+            <h2>
+              Name: {user.first_name} {user.last_name}
+            </h2>
+            <p>Email: {user.email}</p>
+          </>
+        )}
       </div>
 
       <div style={{ marginLeft: "42%" }}>
